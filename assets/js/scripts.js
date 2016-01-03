4a8bcaf672332b3b8fecaa7c118da15bb56f4e4c
kranthikiran01 <guduru.kranthikiran@gmail.com>
2016-01-03 14:46:17 +0530

    Site under construction

---
+++ assets/js/scripts.js
@@ -0,0 +1,66 @@
+/*
+ Author: Ukieweb
+ Template: Egg (Coming Soon)
+ Version: 1.0
+ URL: http://themeforest.net/user/UkieWeb
+ */
+
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+/*
+ ----------------------------------------------------------------------
+ Preloader
+ ----------------------------------------------------------------------
+ */
+    $(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+
+
+/*
+ ----------------------------------------------------------------------
+ Nice scroll
+ ----------------------------------------------------------------------
+ */
+    $("html").niceScroll({
+        cursorcolor: '#fff',
+        cursoropacitymin: '0',
+        cursoropacitymax: '1',
+        cursorwidth: '2px',
+        zindex: 999999,
+        horizrailenabled: false,
+        enablekeyboard: false
+    });
+
+
+/*
+ ----------------------------------------------------------------------
+ Watch
+ ----------------------------------------------------------------------
+ */
+    if($.find('#watch')[0]) {
+
+        $('#watch').countDown({
+            targetDate: {
+                'day': 		1,
+                'month': 	2,
+                'year': 	2016,
+                'hour': 	12,
+                'min': 		0,
+                'sec': 		0
+            },
+            omitWeeks: true
+        });
+        //enter the count down date using the format year, month, day, time: hour, min, sec
+
+    }
+
+
+
+
+
+
+}); // End $(document).ready(function(){
